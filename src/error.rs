c0a50ec8a8abbf84cdfaf84393113b4ac41c077c
MaxXSoft <x@MaxXSoft.net>
2022-02-22 18:27:13 +0800

    (error, parser) removed module `error` and `parser`

--- src/error.rs
+++
@@ -1,5 +0,0 @@
-/// Error type for the current crate.
-pub enum Error {
-  /// Parser error.
-  Parser,
-}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+mod statistics;
+
+use crate::statistics::Statistics;
+use lalrpop_util::lalrpop_mod;
+
+lalrpop_mod! {
+  #[allow(clippy::all)]
+  sysy
+}
+
 fn main() {
   println!("Hello, world!");
+}
+
+/// Error type for the current crate.
+pub enum Error {
+  /// Parser error.
+  Parser,
+}
+
+/// Parses the given SysY program.
+/// Returns statistics about the program.
+pub fn parse(program: &str) -> Result<Statistics, Error> {
+  let mut stats = Statistics::default();
+  sysy::CompUnitParser::new()
+    .parse(&mut stats, program)
+    .map_err(|_| Error::Parser)?;
+  Ok(stats)
 }
